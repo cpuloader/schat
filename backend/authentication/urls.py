bb5123f33a1c82fe70447ef8406ac0a30e556ad9
Petr <bipsoup@gmail.com>
2020-07-24 11:19:15 +0300

    jwt from cookie 1

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from .views import CookieJSONWebTokenAPIView
 
 urlpatterns = [
     url(r'^login/', obtain_jwt_token),
     url(r'^token-refresh/', refresh_jwt_token),
-    url(r'^token-verify/', verify_jwt_token),
+    url(r'^token-verify/', CookieJSONWebTokenAPIView.as_view()),
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -11,6 +11,8 @@ from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework_jwt.serializers import VerifyJSONWebTokenSerializer
+from rest_framework_jwt.settings import api_settings
 
 from django.conf import settings
 from authentication.models import Account, AvatarImage
@@ -18,6 +20,8 @@ from authentication.permissions import IsAccountOwner, IsAuthorOfAvatar
 from authentication.serializers import AccountSerializer, AvatarImageSerializer
 from schat.auth_classes import CsrfExemptSessionAuthentication
 from mymiddleware.activeuser_middleware import get_user_jwt
+
+jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
 
 class AuthRegister(APIView):
     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
@@ -109,3 +113,50 @@ class AvatarViewSet(viewsets.ModelViewSet):
             parent.avatarimage.delete()
             #print('old avatar deleted!')
         serializer.save(author=self.request.user)
+
+
+class CookieJSONWebTokenAPIView(APIView):
+    permission_classes = ()
+    authentication_classes = ()
+    serializer_class = VerifyJSONWebTokenSerializer
+
+    def get_serializer_context(self):
+        return {
+            'request': self.request,
+            'view': self,
+        }
+
+    def get_serializer_class(self):
+        assert self.serializer_class is not None, (
+            "'%s' should either include a `serializer_class` attribute, "
+            "or override the `get_serializer_class()` method."
+            % self.__class__.__name__)
+        return self.serializer_class
+
+    def get_serializer(self, *args, **kwargs):
+        """
+        Return the serializer instance that should be used for validating and
+        deserializing input, and for serializing output.
+        """
+        serializer_class = self.get_serializer_class()
+        kwargs['context'] = self.get_serializer_context()
+        return serializer_class(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.object.get('user') or request.user
+            token = serializer.object.get('token')
+            response_data = jwt_response_payload_handler(token, user, request)
+            response = Response(response_data)
+            if api_settings.JWT_AUTH_COOKIE:
+                expiration = (datetime.utcnow() +
+                              api_settings.JWT_EXPIRATION_DELTA)
+                response.set_cookie(api_settings.JWT_AUTH_COOKIE,
+                                    token,
+                                    expires=expiration,
+                                    httponly=True)
+            return response
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
