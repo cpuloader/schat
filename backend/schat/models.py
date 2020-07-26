e7a09bc62cb281668c111ec868f59e12fa95f1f2
cpuloader <bipsoup@gmail.com>
2020-07-26 11:59:35 +0300

    ws setup

--- backend/schat/models.py
+++ backend/schat/models.py
@@ -5,6 +5,8 @@ import requests
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from authentication.models import Account
 
@@ -44,24 +46,20 @@ class Message(models.Model):
                 'encrypted': self.encrypted
                }
 
-    def save(self, *args, **kwargs):
-        created = self.message is not None
-        super(Message, self).save(*args, **kwargs)
-        if settings.USE_WEBSOCKET:
-            send_to_receivers(self, created)
 
-
-def send_to_receivers(message, created):
+@receiver(post_save, sender=Message)
+def new_message_handler(**kwargs):
+    message = kwargs['instance']
     room = message.room
     receivers = room.members.all()
     s = requests.Session()
-    if created:
+    if kwargs['created']:
         for receiver in receivers:
             if receiver.pk != message.author.pk:
                 url = "{0}/{1}".format(settings.PUB_ENDPOINT, receiver.pk)
-                print('created - pub to', receiver.pk, url);
-                s.post(url, json=json.dumps(message.as_dict()))
-    if not created:
-        url = "{0}/{1}".format(settings.PUB_ENDPOINT, receiver.pk)
-        print('pub to', message.author.pk, url);
-        s.post(url, json=json.dumps(message.as_dict()))
+                #print('created - pub to', receiver.pk, url);
+                s.post(url, data=json.dumps(message.as_dict()))
+    if not kwargs['created']:
+        url = "{0}/{1}".format(settings.PUB_ENDPOINT, message.author.pk)
+        #print('pub to', message.author.pk, url);
+        s.post(url, data=json.dumps(message.as_dict()))
